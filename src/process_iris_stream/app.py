1d32c88d0449c13b71635853fdbe507e374b3e01
Lucas Bellomo <lbellomo@gmail.com>
2021-07-15 11:27:49 -0300

    add processing logic

--- src/process_iris_stream/app.py
+++ src/process_iris_stream/app.py
@@ -1,12 +1,55 @@
 import json
 import logging
+from typing import List
 
-import sklearn
+import boto3
+
+from dummy_model import Model
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-logger.info(f"sklearn version: {sklearn.__version__}")
+model = Model()
+iris_keys = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+
+dynamodb = boto3.resource("dynamodb", region_name="sa-east-1")
+table = dynamodb.Table("iris_table")
+
+
+def create_output(status_code: int, message: str) -> dict:
+    logger.info(message)
+    return {
+        "statusCode": status_code,
+        "body": json.dumps(
+            {
+                "message": message,
+            }
+        ),
+    }
+
+
+def parse_records(records: List[dict]) -> List[dict]:
+    """Extract and parse the items from the event's records."""
+    items = list()
+    for record in records:
+        if record["eventName"] != "INSERT":
+            continue
+
+        new_image = record["dynamodb"]["NewImage"]
+
+        item = dict()
+        for k, dict_v in new_image.items():
+
+            # get the first value from the dict
+            v = [i for i in dict_v.values()][0]
+
+            if k != "id":
+                # try
+                v = float(v)
+
+            item[k] = v
+        items.append(item)
+    return items
 
 
 def lambda_handler(event, context):
@@ -24,16 +67,26 @@ def lambda_handler(event, context):
     ------
     API Gateway Lambda Proxy Output Format: dict
     """
+    items = parse_records(event["Records"])
 
-    logger.info(f"event: {event}")
+    for item in items:
+        # make prediction
+        X = [[item[k] for k in iris_keys]]
+        y = model.predict(X)[0]
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+        # update item
+        try:
+            table.update_item(
+                Key={"id": item["id"]},
+                UpdateExpression="set pred_target=:y",
+                ExpressionAttributeValues={
+                    ":y": y,
+                },
+                ReturnValues="UPDATED_NEW",
+            )
+        except Exception as e:
+            logger.error(f"Error, can't update item: {e}")
+            return create_output(500, "Internal error: can't update item in table.")
 
     return {
         "statusCode": 200,
