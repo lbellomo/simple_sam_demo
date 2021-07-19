f7f5a71a5d91a85db9f43805ea7566f3b04d7ec6
Lucas Bellomo <lbellomo@gmail.com>
2021-07-19 02:55:44 -0300

    remove read function

--- diagram/diagram.py
+++ diagram/diagram.py
@@ -6,19 +6,15 @@ from diagrams.aws.database import DynamodbTable
 with Diagram("Arquitecture"):
 
     lambda_write = LambdaFunction("WriteIrisFunction")
-    lambda_read = LambdaFunction("ReadIrisFunction")
     lambda_process = LambdaFunction("ProcessIrisDynamoDBStream")
 
     dynamo_table = DynamodbTable("IrisTable")
 
     api_endpoint_write = APIGatewayEndpoint("Endpoint write")
-    api_endpoint_read = APIGatewayEndpoint("Endpoint read")
 
     (
         api_endpoint_write
         >> lambda_write
         >> dynamo_table
-        >> lambda_read
-        >> api_endpoint_read
     )
     dynamo_table >> lambda_process >> dynamo_table
